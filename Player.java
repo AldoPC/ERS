b8148cb15ca73bf8de71e0ac036c98350826f81b
AldoPC <aldoponcedelacruz@gmail.com>
2019-04-21 22:52:56 -0500

    Update Player.java
    add sets and gets

--- Player.java
+++ Player.java
@@ -235,6 +235,21 @@ public class Player implements CharacterInterface{
 		System.out.println(name +"s health has increased to " + hp);
 	}
 
+	public void setX(int x){
+		this.x=x;
+	}
+
+	public int getX(){
+		return x;
+	}
+	public void setY(int y){
+		this.y=y;
+	}
+
+	public int getY(){
+		return y;
+	}
+
 
 
 
